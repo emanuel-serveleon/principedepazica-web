082d6f9fd1876051ab58d931779db0b35f8292b8
Emanuel Serveleon <serveleonemanuel@gmail.com>
2024-10-14 23:20:14 -0500

    feat: add title and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import './globals.css';
 
+export const metadata = {
+  title: 'Iglesia Cristiana del Príncipe de Paz',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
