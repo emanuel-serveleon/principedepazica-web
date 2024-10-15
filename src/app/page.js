eaa3b7330ee4775daccdb08312080c7591b95992
Emanuel Serveleon <serveleonemanuel@gmail.com>
2024-10-15 18:56:53 -0500

    feat: add "reuniones semanales" section

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,9 @@
+import img1 from './assets/celebracion.jpg';
+import img2 from './assets/jovenes.jpg';
+import img3 from './assets/escuela.jpg';
+
+import Image from 'next/image';
+
 export default function Page() {
   return (
     <>
@@ -7,6 +13,41 @@ export default function Page() {
         </h1>
         <p className="text-center text-2xl">Nacidos del corazón de Dios</p>
       </section>
+
+      <section className="bg-slate-100 py-24">
+        <h2 className="mb-10 text-center text-4xl font-bold">
+          Reuniones semanales
+        </h2>
+        <div className="flex flex-wrap justify-center gap-10">
+          <ReunionCard
+            name="Culto de Celebración"
+            date="Domingo 6:30 p. m."
+            image={img1}
+          />
+          <ReunionCard
+            name="Culto de Jóvenes"
+            date="Sábado 6:30 p. m."
+            image={img2}
+          />
+          <ReunionCard
+            name="Escuela de Entrenamiento Ministerial"
+            date="Martes 7:00 p. m."
+            image={img3}
+          />
+        </div>
+      </section>
     </>
+  );
+}
+
+function ReunionCard(props) {
+  return (
+    <div className="w-96 bg-white">
+      <Image src={props.image} width={384} height={384} alt="hola" />
+      <div className="p-2">
+        <p className="text-center font-bold">{props.name}</p>
+        <p className="text-center">{props.date}</p>
+      </div>
+    </div>
   );
 }
