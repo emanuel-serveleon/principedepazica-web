fbb6a1c3c781160660f807820a64ad3b742b6a30
Emanuel Serveleon <serveleonemanuel@gmail.com>
2024-10-15 18:36:54 -0500

    feat: add hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,12 @@
 export default function Page() {
-  return <h1>Iglesia Cristiana del Príncipe de Paz</h1>;
+  return (
+    <>
+      <section className="flex h-screen flex-col justify-center bg-slate-200 p-10">
+        <h1 className="m-5 text-center text-5xl font-bold">
+          Iglesia Cristiana del Príncipe de Paz
+        </h1>
+        <p className="text-center text-2xl">Nacidos del corazón de Dios</p>
+      </section>
+    </>
+  );
 }
