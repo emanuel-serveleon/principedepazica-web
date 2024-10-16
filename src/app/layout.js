8c1063dc3db19b0bea6cfba792cf57abdcaba008
Emanuel Serveleon <serveleonemanuel@gmail.com>
2024-10-15 23:23:11 -0500

    fix: prevent header from covering content

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="bg-slate-200">
+      <body className="bg-slate-200 pt-16">
         <Header />
         {children}
         <Footer />
