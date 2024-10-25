98c4b458ca1486c1dbae052ebbfef906d199157b
Emanuel Serveleon <serveleonemanuel@gmail.com>
2024-10-25 15:54:26 -0500

    fix: make the footer stay at the bottom
    This was done by making the body a flex container, and wrapping the main
    content inside of a main tag with flex auto

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,9 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="bg-slate-200 pt-16">
+      <body className="flex min-h-screen flex-col justify-between bg-slate-200 pt-16">
         <Header />
-        {children}
+        <main className="flex-auto">{children}</main>
         <Footer />
         <Credit />
       </body>
